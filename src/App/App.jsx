fecf18c8cee9d15376f050746dfd4354347a1682
sachitha <sachitha@meetrix.io>
2021-01-28 01:07:30 +0530

    login/signup changes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../_styles/Styles.css'
@@ -10,51 +10,48 @@ import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 import { Alert, Spinner } from "react-bootstrap";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+const App = (props) => {
 
-        history.listen((location, action) => {
-            // clear alert on location change
-            this.props.clearAlerts();
-        });
-    }
+    const { alert, loader, clearAlerts } = props;
+    useEffect(() => {
+    history.listen((location, action) => {
+        // clear alert on location change
+        clearAlerts();
+    });
+    }, [])
 
-    render() {
-        const { alert, loader } = this.props;
-        return (
-            <div>
+    return (
+        <div>
 
-                <Router history={history}>
-                    {alert.message &&
-                        <Alert className="alert-box" variant={alert.type == "alert-danger" ? 'danger' : 'success'} dismissible onClose={() => this.props.clearAlerts()}>
-                            <Alert.Heading>{alert.type == "alert-danger" ? 'Error' : 'Success'}!  {alert.message}</Alert.Heading>
-                        </Alert>
-                    }
-                    {loader.loading && loader.count != 0 &&
-                        <div className="custom-loader">
-                            <Spinner animation="grow" variant="primary" />
-                            <Spinner animation="grow" variant="secondary" />
-                            <Spinner animation="grow" variant="success" />
-                            <Spinner animation="grow" variant="danger" />
-                            <Spinner animation="grow" variant="warning" />
-                            <Spinner animation="grow" variant="info" />
-                            <Spinner animation="grow" variant="light" />
-                            <Spinner animation="grow" variant="dark" />
-                        </div>
-                    }
-                    <Switch>
-                        <PrivateRoute path="/university" component={HomePage} />
-                        <PrivateRoute path="/Department" component={HomePage} />
-                        <PrivateRoute path="/Student" component={HomePage} />
-                        <Route path="/login" component={LoginPage} />
-                        <Route path="/register" component={RegisterPage} />
-                        <Redirect from="*" to="/login" />
-                    </Switch>
-                </Router>
-            </div>
-        );
-    }
+            <Router history={history}>
+                {alert.message &&
+                    <Alert className="alert-box" variant={alert.type == "alert-danger" ? 'danger' : 'success'} dismissible onClose={() => this.props.clearAlerts()}>
+                        <Alert.Heading>{alert.type == "alert-danger" ? 'Error' : 'Success'}!  {alert.message}</Alert.Heading>
+                    </Alert>
+                }
+                {loader.loading && loader.count != 0 &&
+                    <div className="custom-loader">
+                        <Spinner animation="grow" variant="primary" />
+                        <Spinner animation="grow" variant="secondary" />
+                        <Spinner animation="grow" variant="success" />
+                        <Spinner animation="grow" variant="danger" />
+                        <Spinner animation="grow" variant="warning" />
+                        <Spinner animation="grow" variant="info" />
+                        <Spinner animation="grow" variant="light" />
+                        <Spinner animation="grow" variant="dark" />
+                    </div>
+                }
+                <Switch>
+                    <PrivateRoute path="/university" component={HomePage} />
+                    <PrivateRoute path="/Department" component={HomePage} />
+                    <PrivateRoute path="/Student" component={HomePage} />
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/register" component={RegisterPage} />
+                    <Redirect from="*" to="/login" />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 function mapState(state) {
--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,105 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { RoleTypeCodes } from '../_constants';
 import { userActions } from '../_actions';
 
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
+const LoginPage = (props) => {
 
-        // reset login status
-        this.props.logout();
+    const { loggingIn, logout, login } = props;
 
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false,
-            user_role_id : undefined,
-        };
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+    const [user_role_id, setUser_role_id] = useState(undefined);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    useEffect(() => {
+        logout();
+    }, [])
 
-    handleChange(e) {
+    const handleChange = (e) => {
         const { name, value } = e.target;
-        this.setState({ [name]: value });
+
+        if (name === 'username') setUsername(value);
+        else if (name === 'password') setPassword(value);
+        else if (name === 'user_role_id') setUser_role_id(value);
+
     }
 
-    handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        this.setState({ submitted: true });
-        const { username, password, user_role_id } = this.state;
+        setSubmitted(true);
         if (username && password && user_role_id) {
-            this.props.login(username, password, user_role_id);
+            login(username, password, user_role_id);
         }
     }
 
-    render() {
-        const { loggingIn } = this.props;
-        const { username, password, submitted, user_role_id } = this.state;
-        return (
-            <div className="row login-container">
-            <div className="col-md-6 login-box">
-                <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                            <div className="help-block">Username is required</div>
+    return (
+        <div className="row login-container">
+        <div className="col-md-6 login-box">
+            <h2>Login</h2>
+                <form name="form" onSubmit={handleSubmit}>
+                <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                    <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={handleChange} />
+                    {submitted && !username &&
+                        <div className="help-block">Username is required</div>
+                    }
+                </div>
+                <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                    <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={handleChange} />
+                    {submitted && !password &&
+                        <div className="help-block">Password is required</div>
+                    }
+                </div>
+                <div className={'form-group' + (submitted && !user_role_id ? ' has-error' : '')}>
+                    <label htmlFor="password"></label>
+                        <select className="form-control" name="user_role_id" value={user_role_id} onChange={handleChange}>
+                        
+                            <option value="0">
+                            Select Role
+                            </option>
+                                
+
+                        {
+                                RoleTypeCodes.map((option, index) =>  {
+
+                                return(
+                                    
+                                    <option value={option.id} key={index}>
+                                        {option.value}
+                                    </option>
+
+                                    )
+                            })
+
                         }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user_role_id ? ' has-error' : '')}>
-                        <label htmlFor="password"></label>
-                        <select className="form-control" name="user_role_id" value={user_role_id} onChange = {this.handleChange}>
-                            
-                             <option value="0">
-                                Select Role
-                             </option>
-                                 
+                    </select>
+                    {submitted && !user_role_id &&
+                        <div className="help-block">User role is required</div>
+                    }
+                </div>
 
-                            {
-                                 RoleTypeCodes.map((option, index) =>  {
+                <div className="form-group">
+                    <button className="btn btn-primary">Login</button>
+                    {loggingIn &&
+                        <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                    }
+                    <Link to="/register" className="btn btn-link">Register</Link>
+                </div>
+            </form>
+        </div>
+        </div>
 
-                                    return(
-                                        
-                                        <option value={option.id} key={index}>
-                                            {option.value}
-                                        </option>
-
-                                        )
-                                })
-
-                            }
-                        </select>
-                        {submitted && !user_role_id &&
-                            <div className="help-block">User role is required</div>
-                        }
-                    </div>
-
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        {loggingIn &&
-                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                        <Link to="/register" className="btn btn-link">Register</Link>
-                    </div>
-                </form>
-            </div>
-            </div>
-
-        );
-    }
+    );
 }
 
 function mapState(state) {
